484bda4bc72e20f8c5dcba845e5c73088a3336ad
Realworrior <kemboinic@gmail.com>
2026-04-21 20:00:05 +0300

    chore: cleaned up diagnostic scripts and redundant folders

--- fetch-error-text.js
+++
@@ -1,9 +0,0 @@
-import * as cheerio from 'cheerio';
-async function fetchErrorText() {
-  const url = 'https://react.dev/errors/310';
-  const response = await fetch(url);
-  const text = await response.text();
-  const $ = cheerio.load(text);
-  console.log($('p').map((i, el) => $(el).text()).get().join('\n'));
-}
-fetchErrorText();
