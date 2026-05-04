5fc094e2d276b10635c2175969101529244244e2
Realworrior <kemboinic@gmail.com>
2026-05-04 14:40:34 +0300

    Fix ReferenceError: useEffect is not defined in Resources.tsx

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SmartAssistant } from "../components/SmartAssistant";
 import { useSupabaseData } from "../context/SupabaseDataContext";
 
